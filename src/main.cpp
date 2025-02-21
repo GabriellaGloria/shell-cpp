6a4a69cdacbaa807f87287bb5d7de8afc4164d50
Gabriella <gabriella@u.nus.edu>
2025-02-22 01:35:22 +0800

    Add echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string>
+
+std::string trim(std::string str, int start, int ends){
+    if ((ends - start + 1) <= 0 || (ends - start + 1) > str.length()) {
+        return ""; 
+    }
+    return str.substr(start, ends - start + 1);
+} 
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,9 +18,12 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    
-    if(input == "exit 0") return 0;
-    
-    std::cout << input << ": command not found" << std::endl;
+    if(trim(input, 0, 3) == "echo"){
+      std::cout << trim(input, 5, input.length() - 1) << std::endl;
+    } else if(input == "exit 0"){
+      return 0;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
 }
